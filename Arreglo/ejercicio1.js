f469baae0dad413d0850c4b2af30665e781470e0
JHOON231 <jhondavis.quinterot@gmail.com>
2026-05-27 23:00:00 -0500

    commit inicial1

---
+++ Arreglo/ejercicio1.js
@@ -0,0 +1,64 @@
+let notas = []
+
+Eleagregarmentos = function(){
+    notas.push(5);
+    notas.push(10);
+    console.log(notas.length);
+}
+  
+function calcularPromedio(){
+    let sumaNotas = 0;
+    let promedio;
+    let notaR;
+
+    for(let indice = 0; indice < notas.length; indice++){
+        notaR = notas[indice];
+        sumaNotas += notaR;
+    }
+
+    promedio = sumaNotas / notas.length;
+
+    return promedio;
+}
+
+function recuperarArreglo(){
+    let notaR;
+    for(let indice = 0; indice<notas.length; indice++){
+        notaR = notas[indice];
+        console.log(notaR);
+    }
+}
+
+generarTabla = function(){
+    let contenidoTabla = "";
+    let cmpTabla = document.getElementById("divTabla");
+    contenidoTabla += "<table><tr><td>UNO</td></tr>" +
+                      "<tr><td>DOS</td></tr>" +
+                      "</table>";
+    cmpTabla.innerHTML = contenidoTabla;
+}
+
+mostrarNotas = function(){
+    let cmpTabla = document.getElementById("divTabla");
+    let contenidoTabla = "<table><tr><td>NOTA</th></td>";
+    let miNota;
+    for (let i=0; i<notas.length; i++){
+        miNota = notas[i];
+        contenidoTabla += "<tr><td>";
+        contenidoTabla += miNota;
+        contenidoTabla += "</tr></td>"
+    }
+    contenidoTabla += "</table>"
+    cmpTabla.innerHTML = contenidoTabla;
+}
+
+function probarAgregados(){
+   let  notaRecuperada;
+   notaRecuperada=recuperarInt("txtNota");
+   AgregarNotas(notaRecuperada);
+}
+
+AgregarNotas = function(nota){
+    notas.push(nota);
+    mostrarNotas();
+}
---
+++ mate.js
@@ -0,0 +1,13 @@
+function abrirMenu(){
+
+    document
+        .getElementById("sidebar")
+        .classList.add("activo");
+}
+
+function cerrarMenu(){
+
+    document
+        .getElementById("sidebar")
+        .classList.remove("activo");
+}
