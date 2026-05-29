dde09aef191ae1c570e086c66980e9184d2e9fe6
JHOON231 <jhondavis.quinterot@gmail.com>
2026-05-28 23:49:25 -0500

    agregue el test

---
+++ preguntas.js
@@ -0,0 +1,9 @@
+function correcta(boton){
+    boton.style.background = "green";
+    boton.innerHTML += " ✅";
+}
+
+function incorrecta(boton){
+    boton.style.background = "red";
+    boton.innerHTML += " ❌";
+}
