ae0f86e092786f3d42b170261b879ff0d6b861d2
cuevolver <sbascueva@gmail.com>
2026-06-10 19:34:17 -0500

    calculadora finalizada

--- mate.js
+++ mate.js
@@ -11,3 +11,153 @@ function cerrarMenu(){
         .getElementById("sidebar")
         .classList.remove("activo");
 }
+
+function calcularInteresSimple(){
+
+    let capital;
+    let tasa;
+    let periodos;
+    let interes;
+    let monto;
+    let contenido;
+    let cmpTabla;
+
+    capital = parseFloat(document.getElementById("txtCapital").value);
+    tasa = parseFloat(document.getElementById("txtTasa").value);
+    periodos = parseInt(document.getElementById("txtPeriodos").value);
+
+    tasa = tasa / 100;
+
+    interes = capital * tasa * periodos;
+    monto = capital + interes;
+
+    contenido = "<table>";
+
+    contenido += "<tr><th>Concepto</th><th>Valor</th></tr>";
+
+    contenido += "<tr><td>Capital</td><td>" + capital.toFixed(2) + "</td></tr>";
+    contenido += "<tr><td>Interés</td><td>" + interes.toFixed(2) + "</td></tr>";
+    contenido += "<tr><td>Monto Final</td><td>" + monto.toFixed(2) + "</td></tr>";
+
+    contenido += "</table>";
+
+    cmpTabla = document.getElementById("divTabla");
+    cmpTabla.innerHTML = contenido;
+}
+
+function calcularInteresCompuesto(){
+
+    let capital;
+    let tasa;
+    let periodos;
+    let monto;
+    let interes;
+    let contenido;
+    let cmpTabla;
+
+    capital = parseFloat(document.getElementById("txtCapital").value);
+    tasa = parseFloat(document.getElementById("txtTasa").value);
+    periodos = parseInt(document.getElementById("txtPeriodos").value);
+
+    tasa = tasa / 100;
+
+    monto =
+        capital *
+        Math.pow(1 + tasa, periodos);
+
+    interes = monto - capital;
+
+    contenido = "<table>";
+
+    contenido += "<tr><th>Concepto</th><th>Valor</th></tr>";
+
+    contenido += "<tr><td>Capital</td><td>" + capital.toFixed(2) + "</td></tr>";
+    contenido += "<tr><td>Interés Generado</td><td>" + interes.toFixed(2) + "</td></tr>";
+    contenido += "<tr><td>Monto Final</td><td>" + monto.toFixed(2) + "</td></tr>";
+
+    contenido += "</table>";
+
+    cmpTabla = document.getElementById("divTabla");
+    cmpTabla.innerHTML = contenido;
+}
+
+function calcularValorFuturo(){
+
+    let capital;
+    let tasa;
+    let periodos;
+    let valorFuturo;
+    let contenido;
+    let cmpTabla;
+
+    capital = parseFloat(document.getElementById("txtCapital").value);
+    tasa = parseFloat(document.getElementById("txtTasa").value);
+    periodos = parseInt(document.getElementById("txtPeriodos").value);
+
+    tasa = tasa / 100;
+
+    valorFuturo =
+        capital *
+        Math.pow(1 + tasa, periodos);
+
+    contenido = "<table>";
+
+    contenido += "<tr>";
+    contenido += "<th>Concepto</th>";
+    contenido += "<th>Valor</th>";
+    contenido += "</tr>";
+
+    contenido += "<tr>";
+    contenido += "<td>Capital Inicial</td>";
+    contenido += "<td>" + capital.toFixed(2) + "</td>";
+    contenido += "</tr>";
+
+    contenido += "<tr>";
+    contenido += "<td>Valor Futuro</td>";
+    contenido += "<td>" + valorFuturo.toFixed(2) + "</td>";
+    contenido += "</tr>";
+
+    contenido += "</table>";
+
+    cmpTabla = document.getElementById("divTabla");
+    cmpTabla.innerHTML = contenido;
+}
+
+function calcularValorPresente(){
+
+    let valorFuturo;
+    let tasa;
+    let periodos;
+    let valorPresente;
+    let contenido;
+    let cmpTabla;
+
+    valorFuturo = parseFloat(document.getElementById("txtFuturo").value);
+    tasa = parseFloat(document.getElementById("txtTasa").value);
+    periodos = parseInt(document.getElementById("txtPeriodos").value);
+
+    tasa = tasa / 100;
+
+    valorPresente =
+        valorFuturo /
+        Math.pow(1 + tasa, periodos);
+
+    contenido = "<table>";
+
+    contenido += "<tr><th>Concepto</th><th>Valor</th></tr>";
+
+    contenido += "<tr>";
+    contenido += "<td>Valor Futuro</td>";
+    contenido += "<td>" + valorFuturo.toFixed(2) + "</td>";
+    contenido += "</tr>";
+
+    contenido += "<tr>";
+    contenido += "<td>Valor Presente</td>";
+    contenido += "<td>" + valorPresente.toFixed(2) + "</td>";
+    contenido += "</tr>";
+
+    contenido += "</table>";
+
+    cmpTabla = document.getElementById("divTabla");
+    cmpTabla.innerHTML = contenido;
+}
